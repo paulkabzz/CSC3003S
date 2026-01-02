c53d2ca651db462ccb6a0c82980732617dd7c550
Paul Kabulu <pkabulu05@gmail.com>
2026-01-02 08:45:25 +0200

    Time: 36 ms (12.60%) | Memory: 46.9 MB (51.79%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -9,7 +9,7 @@ class Solution {
 
         
         for (int i = 0; i < s.length(); i++) {
-            if (m.get(s.charAt(i)).equals(1))  return i;
+            if (m.get(s.charAt(i)) == 1)  return i;
         }
 
         return -1;
