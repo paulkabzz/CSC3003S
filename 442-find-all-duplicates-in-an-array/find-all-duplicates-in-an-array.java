290f100114b91d8b0ee13defba724190efac84ff
Paul Kabulu <pkabulu05@gmail.com>
2026-04-10 11:37:33 +0200

    Time: 22 ms (33.30%) | Memory: 72 MB (20.32%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -8,14 +8,9 @@ class Solution {
                 m.put(nums[i], 1);
             } else {
                 res.add(nums[i]);
-                // m.put(nums[i], m.get(nums[i]) + 1);
             }
         }
-
-        // for (Map.Entry<Integer, Integer> e: m.entrySet()) {
-        //     if (e.getValue() == 2)  res.add(e.getKey());
-        // }
-
+        
         return res;
 
     }
