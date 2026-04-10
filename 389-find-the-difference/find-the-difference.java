91da42832e00a5cabbc7171b33219a0e4eb78d5a
Paul Kabulu <pkabulu05@gmail.com>
2026-04-10 11:26:50 +0200

    Time: 17 ms (6.14%) | Memory: 43.2 MB (44.50%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -6,9 +6,10 @@ class Solution {
     // loop, remove chars feq, if < 0 -> that is added char
 
     // Inefficient - O(n) time + O(2n) = O(n) auxilliary space
+
+    // come back to this
     public char findTheDifference(String s, String t) {
         Map<Character, Integer> m = new HashMap<>(); // chached chars
-        // Map<Character, Integer> n = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             if (!m.containsKey(s.charAt(i))) {
@@ -27,22 +28,6 @@ class Solution {
                 m.put(t.charAt(i), m.get(t.charAt(i)) - 1);
             }
         }
-
-
-        // for (int i = 0; i < t.length(); i++) {
-        //     if (!n.containsKey(t.charAt(i))) {
-        //         n.put(t.charAt(i), 0);
-        //     } else {
-        //         n.put(t.charAt(i), n.get(t.charAt(i)) + 1);
-        //     }
-        // }
-
-        // for (Map.Entry<Character, Integer> entry: n.entrySet()) {
-        //     char _t = entry.getKey();
-
-        //     if (!m.containsKey(_t)) return _t;
-        //     else if (m.get(_t) != entry.getValue()) return _t;
-        // }
 
         return t.charAt(0);
         
