c9c76ccef7f024dd9eba7f59e1f3dadcf7aa00a8
Paul Kabulu <pkabulu05@gmail.com>
2026-08-08 22:49:05 +0200

    Time: 0 ms (100.00%) | Memory: 46.5 MB (87.47%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -13,33 +13,45 @@
 
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> s = new HashSet<>();
+        // Set<ListNode> s = new HashSet<>();
+
+        boolean c = false;
 
         if (head == null || head.next == null) {
             return null;
         }
 
-        while (head != null) {
-            if (s.contains(head)) return head;
-            s.add(head);
-            head = head.next;
-        }
+        // while (head != null) {
+        //     if (s.contains(head)) return head;
+        //     s.add(head);
+        //     head = head.next;
+        // }
 
 
 
-        // ListNode shortPtr = head;
-        // ListNode longPtr = head;
+        ListNode shortPtr = head;
+        ListNode longPtr = head;
 
-        // while (longPtr != null && longPtr.next != null)  {
-        //     prev = longPtr;
-        //     shortPtr = shortPtr.next;
-        //     longPtr = longPtr.next.next;
-        //     if (shortPtr == longPtr) {
-        //         return longPtr;
-        //     }
-        //  }
+        while (longPtr != null && longPtr.next != null)  {
+            shortPtr = shortPtr.next;
+            longPtr = longPtr.next.next;
+            if (shortPtr == longPtr) {
+                c = true;
+                break;
+            }
+         }
 
-         return null;
+         if (c) {
+            longPtr = head;
+            while (shortPtr != longPtr) {
+                shortPtr = shortPtr.next;
+                longPtr = longPtr.next;
+            }
+         } else {
+            return null;
+         }
+
+         return shortPtr;
         
     }
 }
