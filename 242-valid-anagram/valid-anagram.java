56467cb97ed4b6f71fbc4f427371ce08016d90eb
Paul Kabulu <pkabulu05@gmail.com>
2026-08-13 10:34:10 +0200

    Time: 25 ms (5.23%) | Memory: 46.6 MB (16.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,20 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        // store frequency of each character from s in map
-        // every time you come across that character in t, subtract from the freq
-        // if all chars have freq of 0, then valid anagrams
 
         HashMap<Character, Integer> m = new HashMap<>();
 
         if (s.length() != t.length()) return false;
 
         for (int i = 0; i < s.length(); i++) {
-            if (!m.containsKey(s.charAt(i))) {
-                m.put(s.charAt(i), 1);
-            } else {
-                m.put(s.charAt(i), m.get(s.charAt(i)) + 1);
-            }
+            m.put(s.charAt(i), m.getOrDefault(s.charAt(i), 0) + 1);
         }
 
         for (int i = 0; i < t.length(); i++) {
