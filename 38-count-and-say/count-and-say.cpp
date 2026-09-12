c0566d6ee25ee5a1590c946be7e3c88f2d50e780
Paul Kabulu <pkabulu05@gmail.com>
2026-09-12 14:47:56 +0200

    Time: 5 ms (28.91%) | Memory: 10.9 MB (13.92%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,27 +1,5 @@
 class Solution {
 std::string rle(std::string s) {
-    // int* freq = new int[10]{};
-
-    // for (int i = 0; i < s.length(); i++)
-    // {   
-    //     // std::cout << "Index of " << s[i] << " = " << s[i] - 48 << std::endl;
-    //     freq[s[i] - 48]++;
-    // }
-    
-    // std::string res = "";
-
-    // for (int i = 0; i < s.length(); i++) {
-    //     // iterate through freq...
-    //     // check if that freq is non-zero
-    //     // if non-zero, we want to append in correct order. that is, freq[]
-    //     if (freq[s[i] - 48] != 0) {
-    //         res += (std::to_string(freq[s[i] - 48]) + s[i]);
-    //     }
-    // }
-
-    // delete[] freq;
-    // return res;
-
     std::string res = "";
 
     int current_frequency = 1;
