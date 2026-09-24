631388e9be603d5328a96f87470c9117c9692b92
Paul Kabulu <pkabulu05@gmail.com>
2026-09-24 16:11:32 +0200

    Time: 3 ms (31.36%) | Memory: 12.1 MB (14.01%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+
+    std::set<std::vector<int>> set;
+
+    std::vector<std::vector<int>> ps(std::vector<std::vector<int>> &in, std::vector<int> &n) {
+        // use set, return to thsi
+        if (n.size() <= 0) {
+            in.push_back(std::vector<int>());
+            return in;
+        } else {
+            int last = n[n.size() - 1];
+            
+            n.pop_back();
+
+            auto L = ps(in, n);
+            std::vector<std::vector<int>> s;
+
+            for (auto x: L) {
+                x.push_back(last);
+                s.push_back(x);
+            }
+
+            for (auto x: s) {
+                std::sort(x.begin(), x.end());
+
+                if (!set.contains(x))
+                {   
+                    L.push_back(x);
+                    set.insert(x);
+                }
+                
+            }
+            
+            std::sort(L.begin(), L.end());
+            return L;
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        std::vector<std::vector<int>> s;
+        s = ps(s, nums);
+        return s;
+    }   
+};
